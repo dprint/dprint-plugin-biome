23e6023528c1f07fd55d4ee24761eb0785f0b41a
David Sherret <dsherret@gmail.com>
2024-06-30 19:25:31 -0400

    fix

--- deployment/npm/index.test.js
+++ deployment/npm/index.test.js
@@ -4,6 +4,9 @@ const createFromBuffer = require("@dprint/formatter").createFromBuffer;
 const getBuffer = require("./index").getBuffer;
 
 const formatter = createFromBuffer(getBuffer());
-const result = formatter.formatText("file.js", "console.log (   5 )");
+const result = formatter.formatText({
+  filePath: "file.js",
+  fileText: "console.log (   5 )",
+});
 
 assert.strictEqual(result, "console.log(5);\n");
