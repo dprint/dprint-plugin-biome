ffe2bf57b150f0eb4a9994ecd3b2e813731c3562
David Sherret <dsherret@gmail.com>
2024-06-30 19:14:06 -0400

    Update to wasm v4 for conditional css enablement

--- src/wasm_plugin.rs
+++ src/wasm_plugin.rs
@@ -3,14 +3,16 @@ use super::configuration::Configuration;
 
 use dprint_core::configuration::ConfigKeyMap;
 use dprint_core::configuration::GlobalConfiguration;
-use dprint_core::configuration::ResolveConfigurationResult;
 use dprint_core::generate_plugin_code;
+use dprint_core::plugins::CheckConfigUpdatesMessage;
+use dprint_core::plugins::ConfigChange;
 use dprint_core::plugins::FileMatchingInfo;
 use dprint_core::plugins::FormatResult;
 use dprint_core::plugins::PluginInfo;
+use dprint_core::plugins::PluginResolveConfigurationResult;
+use dprint_core::plugins::SyncFormatRequest;
+use dprint_core::plugins::SyncHostFormatRequest;
 use dprint_core::plugins::SyncPluginHandler;
-use dprint_core::plugins::SyncPluginInfo;
-use std::path::Path;
 
 struct BiomePluginHandler;
 
@@ -19,40 +21,49 @@ impl SyncPluginHandler<Configuration> for BiomePluginHandler {
     &mut self,
     config: ConfigKeyMap,
     global_config: &GlobalConfiguration,
-  ) -> ResolveConfigurationResult<Configuration> {
-    resolve_config(config, global_config)
-  }
-
-  fn plugin_info(&mut self) -> SyncPluginInfo {
-    let version = env!("CARGO_PKG_VERSION").to_string();
-    SyncPluginInfo {
-      info: PluginInfo {
-        name: env!("CARGO_PKG_NAME").to_string(),
-        version: version.clone(),
-        config_key: "biome".to_string(),
-        help_url: "https://dprint.dev/plugins/biome".to_string(),
-        config_schema_url: format!(
-          "https://plugins.dprint.dev/dprint/dprint-plugin-biome/{}/schema.json",
-          version
-        ),
-        update_url: Some("https://plugins.dprint.dev/dprint/dprint-plugin-biome/latest.json".to_string()),
-      },
+  ) -> PluginResolveConfigurationResult<Configuration> {
+    let result = resolve_config(config, global_config);
+    let mut file_extensions = vec![
+      "ts".to_string(),
+      "tsx".to_string(),
+      "cts".to_string(),
+      "mts".to_string(),
+      "js".to_string(),
+      "jsx".to_string(),
+      "cjs".to_string(),
+      "mjs".to_string(),
+      "json".to_string(),
+      "jsonc".to_string(),
+    ];
+    if result.config.css_enabled == Some(true) {
+      file_extensions.push("css".to_string());
+    }
+    PluginResolveConfigurationResult {
+      config: result.config,
+      diagnostics: result.diagnostics,
       file_matching: FileMatchingInfo {
-        file_extensions: vec![
-          "css".to_string(),
-          "ts".to_string(),
-          "tsx".to_string(),
-          "cts".to_string(),
-          "mts".to_string(),
-          "js".to_string(),
-          "jsx".to_string(),
-          "cjs".to_string(),
-          "mjs".to_string(),
-          "json".to_string(),
-          "jsonc".to_string(),
-        ],
+        file_extensions,
         file_names: vec![],
       },
+    }
+  }
+
+  fn check_config_updates(&self, _message: CheckConfigUpdatesMessage) -> anyhow::Result<Vec<ConfigChange>> {
+    Ok(Vec::new())
+  }
+
+  fn plugin_info(&mut self) -> PluginInfo {
+    let version = env!("CARGO_PKG_VERSION").to_string();
+    PluginInfo {
+      name: env!("CARGO_PKG_NAME").to_string(),
+      version: version.clone(),
+      config_key: "biome".to_string(),
+      help_url: "https://dprint.dev/plugins/biome".to_string(),
+      config_schema_url: format!(
+        "https://plugins.dprint.dev/dprint/dprint-plugin-biome/{}/schema.json",
+        version
+      ),
+      update_url: Some("https://plugins.dprint.dev/dprint/dprint-plugin-biome/latest.json".to_string()),
     }
   }
 
@@ -62,13 +73,15 @@ impl SyncPluginHandler<Configuration> for BiomePluginHandler {
 
   fn format(
     &mut self,
-    file_path: &Path,
-    file_text: Vec<u8>,
-    config: &Configuration,
-    _format_with_host: impl FnMut(&Path, Vec<u8>, &ConfigKeyMap) -> FormatResult,
+    request: SyncFormatRequest<Configuration>,
+    _format_with_host: impl FnMut(SyncHostFormatRequest) -> FormatResult,
   ) -> FormatResult {
-    let text = std::str::from_utf8(&file_text)?;
-    let maybe_text = super::format_text(file_path, &text, config)?;
+    if request.range.is_some() {
+      return Ok(None); // not implemented
+    }
+
+    let text = String::from_utf8_lossy(&request.file_bytes);
+    let maybe_text = super::format_text(request.file_path, &text, request.config)?;
     Ok(maybe_text.map(|t| t.into_bytes()))
   }
 }
